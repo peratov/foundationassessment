d2d2ef1771f8c2d647366b9fe3e8341e167ea318
Kenneth Anku <kennth.anku@code.berlin>
2023-04-29 15:20:17 +0200

    Comment removed

--- app/app.py
+++ app/app.py
@@ -7,7 +7,6 @@ def create_app():
   app = Flask(__name__)
   app.config.from_object('app.config')
   app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
-  #postgres://blogdb_d2er_user:GA83UzEgej24a9IfbRve0nZrZSOr9icz@dpg-ch5sf94s3fvuobai6tv0-a.frankfurt-postgres.render.com/blogdb_d2er
 
   register_extensions(app)
   register_blueprints(app)
